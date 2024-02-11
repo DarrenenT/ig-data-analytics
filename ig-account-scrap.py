1d46b0476bf77c7b64756a2e2f28aabd58478c3f
DarrenenT <79760560+DarrenenT@users.noreply.github.com>
2024-02-11 15:20:44 +0800

    v1.0

---
+++ ig-account-scrap.py
@@ -0,0 +1,85 @@
+import instaloader
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+import time
+import csv
+
+#for recording time
+total_time = 0
+successful_accounts = 0
+error_accounts = 0
+
+
+# Get instance
+L = instaloader.Instaloader()
+
+df = pd.read_csv('target_accounts_copy.csv')
+ig_accounts = df['Shop'].tolist()
+
+for i, account in enumerate(ig_accounts, start=1):
+    try:
+        start_time = time.time()
+        profile = instaloader.Profile.from_username(L.context, account)
+        current_date = datetime.now()
+
+        # Extract the required informations
+        ig_acc_name = profile.username
+        bio = profile.biography
+        no_of_posts = profile.mediacount
+        no_of_followers = profile.followers
+        one_week_ago = current_date - timedelta(weeks=1)
+        posts_in_last_week = 0
+        likes_in_last_week = 0
+        average_likes_per_post = 0
+        skip_pinpost = 3
+        for post in profile.get_posts():
+            if post.date_utc > one_week_ago:
+                posts_in_last_week += 1
+                likes_in_last_week += post.likes
+                time.sleep(1)  # sleep for 1 second
+            else :
+                skip_pinpost -= 1
+                if skip_pinpost == 0:
+                    break
+
+        if posts_in_last_week != 0:
+            average_likes_per_post = likes_in_last_week / posts_in_last_week
+
+        # Open the CSV file in write mode
+        with open('instagram_data_all.csv', 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+
+            # Write the headers only if the file is empty
+            if os.stat('instagram_data_all.csv').st_size == 0:
+                writer.writerow(
+                    ["ig acc name",
+                    "date of scraping", 
+                    "bio", 
+                    "no. of post", 
+                    "no. of follower", 
+                    "posts in last week", 
+                    "average likes per posts"])
+
+            # Write the data
+            writer.writerow([ig_acc_name, 
+                            current_date.strftime('%Y-%m-%d'), 
+                            bio, 
+                            no_of_posts, 
+                            no_of_followers, 
+                            posts_in_last_week, 
+                            average_likes_per_post])
+
+        time_taken = time.time() - start_time
+        total_time += time_taken
+        successful_accounts += 1
+        print(f"Processed {account},{i} out of {len(ig_accounts)}, took {time_taken} seconds.")
+
+    except instaloader.exceptions.InstaloaderException:
+        error_accounts += 1
+        print(f"Error processing account: {account}")
+        continue
+
+average_time_taken = total_time / successful_accounts if successful_accounts != 0 else 0
+print(f"Processed {successful_accounts + error_accounts} accounts in {total_time} seconds, average time taken per account: {average_time_taken} seconds.")
+print("Successful: ", successful_accounts, "Error: ", error_accounts)
